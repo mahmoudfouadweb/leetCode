49e881313383a0ad230e939307b25f076144a411
Mahmoud Fouad <mfapolice@gmail.com>
2022-11-09 22:49:38 +0200

    done romant to number

---
+++ numeralRoman.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+const intToRoman = (num) => {
+  const sym = {
+    1: "I",
+    5: "V",
+    10: "X",
+    50: "L",
+    100: "C",
+    500: "D",
+    1000: "M",
+  };
+
+  const M = ["", "M", "MM", "MMM"];
+    const C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    const I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    return M[Math.floor(num / 1000)] + C[Math.floor((num % 1000) / 100)] + X[Math.floor((num % 100) / 10)] + I[num % 10];
+};
+intToRoman(1955);
