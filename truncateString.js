7df2840a0b4bea944646ef6bf0646760c74e326a
Mahmoud Fouad <mfapolice@gmail.com>
2022-11-11 04:16:11 +0200

    add requirements

---
+++ truncateString.js
@@ -0,0 +1,7 @@
+function truncateString(str, num) {
+  // if str.length > num >> str + ...
+  // else return str
+  return str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
