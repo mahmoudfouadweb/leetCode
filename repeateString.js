36befe846e3177af549f711cf8730ab38c9c5f2a
Mahmoud Fouad <mfapolice@gmail.com>
2022-11-11 04:12:44 +0200

    add truncateString

--- repeateString.js
+++ repeateString.js
@@ -1,9 +1,18 @@
+/*
+Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+*/
+
 function repeatStringNumTimes(str, num) {
-  
-  
-  // if empty num not positive 
-  if(!num) return '';
-  return str;
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result += str;
+  }
+  console.log(result);
+  // if empty num not positive
+  if (!num) return "";
+  return result;
 }
 
 repeatStringNumTimes("abc", 3);
+repeatStringNumTimes("*", 8);
+repeatStringNumTimes("abc", -2);
