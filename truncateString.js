bd37dfc7aa397c8a243ea4a684f02281d98bfd98
Mahmoud Fouad <mfapolice@gmail.com>
2022-11-11 05:25:10 +0200

    done

--- truncateString.js
+++ truncateString.js
@@ -1,10 +1,31 @@
 function truncateString(str, num) {
   // if str.length > num >> str + ...
   // else return str
-  for (let i = 0; i <= num; i++){
-    
+  let result = "";
+  if (str.length > num) {
+    for (let i = 0; i < num; i++) {
+      result += str[i];
+    }
+    console.log(result);
+    return (result = `${result}...`);
   }
-  return str;
-}
 
+  if (str.length <= num) {
+    for (let i = 0; i < str.length; i++) {
+      result += str[i];
+    }
+    console.log(result);
+    return result;
+  }
+}
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+truncateString("Peter Piper picked a peck of pickled peppers", 11);
+truncateString(
+  "A-tisket a-tasket A green and yellow basket",
+  "A-tisket a-tasket A green and yellow basket".length
+);
+truncateString(
+  "A-tisket a-tasket A green and yellow basket",
+  "A-tisket a-tasket A green and yellow basket".length + 2
+);
+truncateString("A-", 1);
