a7996947a2b6dbe6a82cb067beea7ad11b9f54ee
Mahmoud Fouad <mfapolice@gmail.com>
2022-11-11 06:06:32 +0200

    done

--- findElement.js
+++ findElement.js
@@ -1,12 +1,19 @@
 function findElement(arr, func) {
-  let num = 0;
+  let num = 8;
+
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num) === true) return (num = arr[i]);
+    if (func(num) === false)  num = undefined
+    
+  }
   return num;
 }
 
-findElement([1, 2, 3, 4], (num) => num % 2 === 0);
 findElement([1, 3, 5, 8, 9, 10], function (num) {
   return num % 2 === 0;
 });
+findElement([1, 2, 3, 4], (num) => num % 2 === 0);
 findElement([1, 3, 5, 9], function (num) {
   return num % 2 === 0;
 });
