85b82c0948b768d8ffbb57a18aa3cd650a6ae7f3
Mahmoud Fouad <mfapolice@gmail.com>
2022-11-12 20:01:32 +0200

    first requirement

--- getIndexToIns.js
+++ getIndexToIns.js
@@ -8,21 +8,24 @@ Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has
 
 function getIndexToIns(arr, num) {
   // sort the first argument
-  let current = arr[0];
+  
+  let location;
   for (let i = 0; i < arr.length; i++) {
-     let next = arr[i + 1];
-    if (next < current) {
-      current = next;
-      next = current;
-      current = arr[i];
-      next = arr[i + 1];
-      console.log(arr);
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
     }
   }
-
   // get the location of the second argument
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) location = arr[i];
+    else location = num;
+  }
 
-  return num;
+  return location;
 }
 
-getIndexToIns([70, 40, 60], 50);
+getIndexToIns([70, 40, 60, 1.5, 10, 5, 1], 50);
