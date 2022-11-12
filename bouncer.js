8cd0b3c812fb1b7523a3fae0823c32295fc14b8d
Mahmoud Fouad <mfapolice@gmail.com>
2022-11-12 05:50:08 +0200

    working

--- bouncer.js
+++ bouncer.js
@@ -1,4 +1,19 @@
+/*
+Falsy Bouncer
+Remove all falsy values from an array. Return a new array; do not mutate the original array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Hint: Try converting each value to a Boolean.
+*/
+
 function bouncer(arr) {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    if (arr[i] != false) newArray.push(arr[i]);
+  }
+  console.log(newArray);
   return arr;
 }
 
