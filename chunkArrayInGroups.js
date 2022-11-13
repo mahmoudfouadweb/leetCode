4dc0f804247dfcf8bc8b77ad42abfcd3242670f7
Mahmoud Fouad <mfapolice@gmail.com>
2022-11-13 05:05:50 +0200

    done

--- chunkArrayInGroups.js
+++ chunkArrayInGroups.js
@@ -1,6 +1,20 @@
 function chunkArrayInGroups(arr, size) {
-  
-  return arr;
+  let twoDimensionArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    twoDimensionArray.push(arr.splice(0, size));
+    if (arr.length <= 3 && arr.length)
+      twoDimensionArray.push(arr.splice(0, size));
+    if (arr.length <= 1 && arr.length) twoDimensionArray.push(arr.splice(0, 1));
+  }
+
+  console.log("twoDimensionArray", twoDimensionArray);
+  return twoDimensionArray;
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);
