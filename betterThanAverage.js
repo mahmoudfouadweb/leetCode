761c210af6337ed6f1e8545a4874a10e3623097d
Mahmoud Fouad <mfapolice@gmail.com>
2022-11-16 08:07:43 +0200

    betterThanAverage finally done

--- betterThanAverage.js
+++ betterThanAverage.js
@@ -1,5 +1,9 @@
 function betterThanAverage(classPoints, yourPoints) {
   // Your code here
-  
+  classPoints.push(yourPoints)
+  const sum = classPoints.reduce((acc, current) => acc + current, 0);
+  console.log(sum / classPoints.length < yourPoints);
+  return sum / classPoints.length < yourPoints;
 }
+
 betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75);
