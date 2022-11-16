e1232809c970ed1f24ad05e3793e5fe8bdf21fab
Mahmoud Fouad <mfapolice@gmail.com>
2022-11-16 06:40:25 +0200

    done

--- hoopCount.js
+++ hoopCount.js
@@ -1,3 +1,10 @@
 function hoopCount (n) {
   //your code goes here    
+ 
+    if(n >= 10)
+    return "Great, now move on to tricks";
+    else if( n < 10) return 'Keep at it until you get it'
+
+  
 }
+hoopCount (11)
